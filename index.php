e44c2faa92d6094c1f9dc10ca248bbf50a13b02b
O1Jack1O <Komoep21@gmail.com>
2019-04-29 18:11:20 +0200

    Установка ..

--- index.php
+++ index.php
@@ -5,13 +5,10 @@
 
 </head>
 <body >
-<?php
-require ('connection.php');
-?>
+<?php require ('connection.php'); ?>
 <?php if(isset($_SESSION['Logged_user'])): ?>
 Вы авторизованы как <?php  echo($_SESSION['Logged_user']['username']);?>
 <hr>
-
 <a href="/logout.php">Выйти</a>
 <?php else :?>
 <a href="singup.php">Регистрация</a><br>
