c07501b805bc75d71333a41b848fec99d4c99696
O1Jack1O <Komoep21@gmail.com>
2019-04-29 23:07:10 +0200

    Вывод таблицы Пользователей.

--- index.php
+++ index.php
@@ -2,14 +2,35 @@
 <html>
 <head>
     <meta  charset="UTF-8">
-
+    <link href="tableStyle.css" rel="stylesheet" type="text/css">
 </head>
 <body >
-<?php require ('connection.php'); ?>
-<?php if(isset($_SESSION['Logged_user'])): ?>
-Вы авторизованы как <?php  echo($_SESSION['Logged_user']['username']);?>
+<?php require ('connection.php');
+$data = mysqli_query($link, "SELECT * FROM users ;")->fetch_all();
+?>
+
+<?php if(isset($_SESSION['Logged_user'])): ?><h2>
+    Здравствуйте <?php  echo $_SESSION['Logged_user']['username'],'!    <a href="/logout.php">Выйти</a></h2>';?>
 <hr>
-<a href="/logout.php">Выйти</a>
+
+<?php
+echo '<table class="table1">';
+echo '<tr>';
+echo '<th>№</th><th>id</th><th>Username</th><th>Password</th><th>PhoneNumber</th>';
+echo '</tr>';
+$i=1;
+foreach($data as $base_key => $base_value){
+    echo '<tr>';
+    echo '<td>', $i++, '</td>';
+    foreach($base_value as $key => $value ){
+        echo '<td>', $value, '</td>';
+    }
+    echo '</tr>';
+}
+echo '</table>';
+echo '<br />';
+?>
+
 <?php else :?>
 <a href="singup.php">Регистрация</a><br>
 <a href="login.php">Авторизация</a>
